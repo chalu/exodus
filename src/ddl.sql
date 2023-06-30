ee228fc3f4004c3b42e948f89644e86e6eff7bc5
Charles Opute Odili <chaluwa@gmail.com>
2023-06-30 13:38:54 +0000

    add topics table

--- src/ddl.sql
+++ src/ddl.sql
@@ -9,3 +9,17 @@ CREATE TABLE "users" (
 
 ALTER TABLE "users" 
 	ADD CONSTRAINT "non_empty_username" CHECK (LENGTH(TRIM("username")) > 0);
+
+
+-- topics
+DROP TABLE IF EXISTS "topics";
+CREATE TABLE "topics" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(30) UNIQUE NOT NULL,
+  "description" VARCHAR(500),
+  "created_at" TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP
+);
+
+ALTER TABLE "topics" 
+	ADD CONSTRAINT "non_empty_topicname" CHECK (LENGTH(TRIM("name")) > 0);
+

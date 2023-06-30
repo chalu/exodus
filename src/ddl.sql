8deb12278b5ce7854e32e4ba193619a95a46e60c
Charles Opute Odili <chaluwa@gmail.com>
2023-06-30 13:39:27 +0000

    add posts table

--- src/ddl.sql
+++ src/ddl.sql
@@ -23,3 +23,29 @@ CREATE TABLE "topics" (
 ALTER TABLE "topics" 
 	ADD CONSTRAINT "non_empty_topicname" CHECK (LENGTH(TRIM("name")) > 0);
 
+
+-- posts
+DROP TABLE IF EXISTS "posts";
+CREATE TABLE "posts" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER,
+  "topic_id" INTEGER NOT NULL,
+  "title" VARCHAR(100) NOT NULL,
+  "url" TEXT,
+  "content" TEXT,
+  "created_at" TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP
+);
+
+ALTER TABLE "posts" 
+	ADD FOReign KEY ("topic_id") references "topics" ON DELETE CASCADE,
+    ADD FOReign KEY ("user_id") references "users" ON DELETE SET NULL,
+    ADD CONSTRAINT "non_empty_posttitle" CHECK (LENGTH(TRIM("title")) > 0),
+    ADD CONSTRAINT "post_cnt_or_url" CHECK (
+      NOT (
+        LENGTH(TRIM("url")) > 0
+      	AND LENGTH(TRIM("content")) > 0
+      )
+    ); 
+
+
+

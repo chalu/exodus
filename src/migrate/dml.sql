f51c1cc991344dbe6b791568acfb822dcee3c6bb
Charles Opute Odili <chaluwa@gmail.com>
2023-07-06 22:58:09 +0000

    migrate users

--- src/migrate/dml.sql
+++ src/migrate/dml.sql
@@ -10,3 +10,23 @@ RESTART IDENTITY;
 -- TODO maybe change topics to title case and replace underscores with space
 INSERT INTO "topics" ("name") SELECT DISTINCT topic FROM "bad_posts";
 
+
+-- 2. migrate users
+--
+-- PS: There are lots of duplicate usernames e.g
+-- SELECT COUNT(users) FROM (SELECT username AS users FROM bad_posts) t1 => 50000 users 
+-- vs
+-- SELECT COUNT(users) FROM (SELECT DISTINCT username AS users FROM bad_posts) t1 => 100 users
+-- Also, 
+-- SELECT unnest(string_to_array(upvotes, ',')) AS users VS SELECT DISTINCT unnest(string_to_array(upvotes, ',')) AS users
+-- results in 249799 vs 1100 users while a smilimar query for downvotes results in 249911 vs 1100 users.
+-- Comsequently, total users (across username, upvotes, downvotes in bad_posts and username in bad_comments)
+-- sits at 11077
+WITH allusers AS (
+    SELECT username FROM bad_posts
+    UNION SELECT unnest(string_to_array(upvotes, ',')) AS username FROM bad_posts
+    UNION SELECT unnest(string_to_array(downvotes, ',')) AS username FROM bad_posts
+    UNION SELECT username FROM bad_comments
+)
+INSERT INTO "users" ("username") SELECT DISTINCT username FROM allusers;
+
